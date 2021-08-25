23b9b677257d2d3e40096e7ec23aab6b2e771e53
토깽이 <happyu9986@gmail.com>
2021-08-25 19:38:44 +0900

    inital commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+import {fundamentalsCollection} from "./data/fundamentals.js";
+import {pwaCollection} from "./data/pwa.js";
+import {buildtoolsCollection} from "./data/build-tools.js";
+import {designAndUXCollection} from "./data/design-and-ux.js";
+
+/**
+ * All available collections.
+ */
+export const collections = [
+	fundamentalsCollection,
+	pwaCollection,
+	buildtoolsCollection,
+	designAndUXCollection,
+];
